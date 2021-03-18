955d280d93a845e6eb06195ab07cec796d0682df
joao-paulo-parity <joao-paulo@parity.io>
2021-03-17 21:17:20 -0300

    fixup

--- src/companion.rs
+++ src/companion.rs
@@ -126,7 +126,7 @@ pub async fn companion_update(
 
 	let owner_remote = "origin";
 	// FIXME !! CHANGE THIS BEFORE DEPLOYING TO PRODUCTION
-	let owner_branch = contributor_branch;
+	let owner_branch = format!("{}_owner", contributor_branch);
 	let owner_remote_branch = format!("{}/{}", owner_remote, owner_branch);
 
 	run_cmd(
